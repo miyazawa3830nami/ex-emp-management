638bf8441ee2d58329d047718bfb617031434fb1
miyazawa <nami.miyazawa@rakus-partners.co.jp>
2021-11-02 14:23:11 +0900

    Administrator.java作成

--- src/main/java/jp/co/sample/domain/Administrator.java
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -2,10 +2,21 @@ package jp.co.sample.domain;
 
 public class Administrator {
 
-	private Integer id;
-	private String name;
-	private String mailAddress;
-	private String password;
+	/**
+	 * 管理者情報を表すドメイン
+	 */
+
+	private Integer id;			//	ID
+	private String name;		//	名前
+	private String mailAddress;	//	メールアドレス
+	private String password;	//	パスワード
+	
+	public Administrator(Integer id, String name, String mailAddress, String password) {
+		this.id = id;
+		this.name = name;
+		this.mailAddress = mailAddress;
+		this.password = password;
+	}
 	
 	public Integer getId() {
 		return id;
