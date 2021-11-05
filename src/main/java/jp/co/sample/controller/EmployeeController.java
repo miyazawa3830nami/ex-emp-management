f464950f10deb921e7c4559030cb3bb387a78d6e
miyazawa <nami.miyazawa@rakus-partners.co.jp>
2021-11-05 09:37:25 +0900

    detailhtml作成

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -3,8 +3,10 @@ package jp.co.sample.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 @Controller
@@ -23,5 +25,16 @@ public class EmployeeController {
 	public String showList(Model model) {
 		model.addAttribute("employeeList",employeeService.showList());
 		return "employee/list";
+	}
+	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		model.addAttribute("employee",employeeService.showDetail(Integer.parseInt(id)));
+		return "employee/detail";
 	}
 }
